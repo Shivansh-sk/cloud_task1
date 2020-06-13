f6c705021afe1e66d6bf3b4cec17992010c0d7ec
Shivansh-sk <59771258+Shivansh-sk@users.noreply.github.com>
2020-06-13 18:42:15 +0530

    test5

--- page.php
+++ page.php
@@ -2,11 +2,10 @@
 <pre>
 <html>
 <body>
-<? php
+<?php
 print "Welcome to Shivansh Khandelwal's page" 
 print `ifconfig`;
 <img src='https://buckettask1.s3.ap-south-1.amazonaws.com/dog.916.jpg' , width='499', height='392'/>
- 
 ?>
 </body>
 <html>
