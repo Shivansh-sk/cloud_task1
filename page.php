a4d12cdf654a09d8dd55caeba49e147ccc4311d5
Shivansh <khandelwalshivansh3@gmail.com>
2020-06-13 17:21:42 +0530

    final1

---
+++ page.php
@@ -0,0 +1,9 @@
+<body bgcolor='#e1e1e1'>
+<pre>
+<? php
+print "Welcome to Shivansh Khandelwal's page" 
+print `ifconfig`;
+<img src='https://buckettask1.s3.ap-south-1.amazonaws.com/dog.916.jpg' , width='499', height='392'/>
+ 
+?>
+</pre>
