28874e1f515863333ef4a658523cd95b2deb407c
Shivansh <khandelwalshivansh3@gmail.com>
2020-06-13 18:22:58 +0530

    last

--- page.php
+++ page.php
@@ -1,9 +1,13 @@
 <body bgcolor='#e1e1e1'>
 <pre>
+<html>
+<body>
 <? php
 print "Welcome to Shivansh Khandelwal's page" 
 print `ifconfig`;
 <img src='https://buckettask1.s3.ap-south-1.amazonaws.com/dog.916.jpg' , width='499', height='392'/>
  
 ?>
+</body>
+<html>
 </pre>
